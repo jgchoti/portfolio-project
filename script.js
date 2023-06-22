8c31c7cc98d4e17749e169851e5b9710031e050d
pnchoti <jgchotirat@gmail.com>
2023-06-22 12:25:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Add event listener to the "Read More" button
 var readMoreElements = document.getElementsByClassName("read-more");
 for (var i = 0; i < readMoreElements.length; i++) {
-    readMoreElements[i].addEventListener("click", function () {
-        var btnText = this;
-        if (btnText.innerHTML === "Hide...") {
-            btnText.innerHTML = "Read More...";
-        } else {
-            btnText.innerHTML = "Hide...";
-        }
-    });
+    (function (index) {
+        readMoreElements[index].addEventListener("click", function () {
+            var btnText = this;
+            if (btnText.innerHTML === "Hide...") {
+                btnText.innerHTML = "Read More...";
+            } else {
+                btnText.innerHTML = "Hide...";
+            }
+        });
+    })(i);
 }
+
+
